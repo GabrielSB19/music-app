01c7e694d72df7c395e2e3a0cbfc6aecf8e9a01c
GabrielSB19 <gasuarez2002@gmail.com>
2024-01-16 10:10:16 -0500

    Songs fetching and list display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["zslpuedlkziptwkznxqz.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
